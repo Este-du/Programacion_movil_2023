5d5c41fde48eaab4d72a2cba91c8e2d0e620120c
Este-du <es.fierro@duocuc.cl>
2023-09-10 21:18:48 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Duoc_Prueba1',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
